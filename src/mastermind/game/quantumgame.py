bcbb9a5f223d65825e34ce9940916e9a1b409fa4
TimVroomans <tim.vroomans@gmail.com>
2020-12-13 16:55:00 +0100

    WIP quantum oracle

--- src/mastermind/game/quantumgame.py
+++ src/mastermind/game/quantumgame.py
@@ -1,17 +1,14 @@
 from abc import ABC
-from itertools import combinations
+from itertools import combinations, permutations
 
 import numpy as np
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 from scipy.special import binom
 
-# Following import is actually necessary for the 'mcrx' gate.
-# noinspection PyUnresolvedReferences
-from qiskit.circuit.library.standard_gates import multi_control_rotation_gates
-
 from experiment.qiskit_experiment import QiskitExperiment
 from experiment.util import filter_at
 from .game import Game
+from mastermind.arithmetic.increm import increment
 
 
 def _sum_filter(index, counts):
@@ -19,83 +16,85 @@ def _sum_filter(index, counts):
 
 
 class QuantumGame(Game, ABC):
-    def __init__(self, turns=10, num_slots=4, pin_amount=8, ask_input=True):
-        self.color_bits = int(np.ceil(np.log2(pin_amount)))
+    def __init__(self, turns=10, num_slots=4, colour_amount=6, ask_input=True):
+        '''
+        Initialises quantum and classical registers for the quantum version of
+        mastermind
 
-        self.q = QuantumRegister(self.color_bits * num_slots + 2)
-        self.b = ClassicalRegister(self.color_bits * num_slots + 2)
-        self.circuit: QuantumCircuit = QuantumCircuit(self.q, self.b)
+        Parameters
+        ----------
+        turns : integer, optional
+            Amount of allowed turns. The default is 10.
+        num_slots : integer, optional
+            Amount of elements in the code. The default is 4.
+        colour_amount : integer, optional
+            Amount of colours available for the code. The default is 6.
+        ask_input : boolean, optional
+            Ask thu user for input. The default is True.
 
+        Returns
+        -------
+        None.
+
+        '''
+        self.amount_colour_bits = int(np.ceil(np.log2(colour_amount)))
+        self.amount_answer_qubits = int(np.ceil(np.log2(num_slots))) + 1
+        
+        # Query register
+        self.q = QuantumRegister(self.amount_colour_bits * num_slots + 2, 'q')
+        
+        # Answer pin registers
+        self.a = QuantumRegister(self.amount_answer_qubits, 'a')
+        self.b = QuantumRegister(self.amount_answer_qubits, 'b')
+        self.classical_a = ClassicalRegister(self.amount_answer_qubits, 'A')
+        self.classical_b = ClassicalRegister(self.amount_answer_qubits, 'B')
+        
+        # Compare qubit register
+        self.COMP = QuantumRegister(1, 'COMP')
+        
+        # Build circuit from registers
+        self.circuit = QuantumCircuit(self.q, self.a, self.b, self.COMP, self.classical_a, self.classical_b)
+        
+        # Set up qiskit experiment
         self.experiment = QiskitExperiment()
+        
+        # Initialise Mastermind
+        super(QuantumGame, self).__init__(turns, num_slots, colour_amount, ask_input)
 
-        super(QuantumGame, self).__init__(turns, num_slots, pin_amount, ask_input)
+    def check_input(self, int_list, sequence):
+        if self.circuit.size() == 0:
+            self._construct_check_circuit(sequence)
 
     def _qc_sequence_chk(self, sequence, circuit, input_mode=False):
         for (pos, num) in enumerate(sequence):
             self.qc_chk(pos, num, circuit, input_mode)
 
     def _construct_check_circuit(self, sequence):
-        self.qc_blacks(sequence)
-        self.qc_whites(sequence)
+        '''
+        Builds the quantum circuit used to obtain results from mastermind
 
-    def check_input(self, int_list, sequence):
-        if self.circuit.size() == 0:
-            self._construct_check_circuit(sequence)
+        Parameters
+        ----------
+        sequence : List
+            Secret sequence as used in mastermind.
 
-        input_circuit = QuantumCircuit(self.q, self.b)
-        self._qc_sequence_chk(int_list, input_circuit, True)
+        Returns
+        -------
+        Check circuit.
 
-        check_circuit = input_circuit.combine(self.circuit)
-
-        shots = 1024
-
-        result = self.experiment.run(check_circuit, shots, optimization=0)
-        blacks = (_sum_filter(1, result.get_counts()) / shots) * self.num_slots
-        whites = (_sum_filter(0, result.get_counts()) / shots) * self.num_slots
-
-        print("Probablity distribution blacks: ", blacks, " and whites:", whites)
-        return int(round(blacks)), int(round(whites))
+        '''
+        # Find all permutations of the secret sequence
+        sequence_permutations = [p for p in permutations(sequence)]
+        
+        for p in sequence_permutations:
+            for q in 
+    
 
     def random_sequence(self):
         # While not part of our project, we could've used our ndie.py to roll an 'pin_amount' sided dice 'num_slots'
         # times to generate our sequence in a quantum way
         return np.random.randint(0, self.pin_amount, size=self.num_slots).tolist()
 
-    def qc_blacks(self, code):
-        for i_p in range(self.num_slots):
-            # Place opening quantum circuit
-            self.qc_chk(i_p, code[i_p], self.circuit)
-
-            # Place mcrx gate
-            controls = [self.q[self.color_bits * i_p + i] for i in range(self.color_bits)]
-            self.circuit.mcrx(np.pi / self.num_slots, controls, self.q[self.color_bits * self.num_slots])
-
-            # Place closing quantum circuit
-            self.qc_chk(i_p, code[i_p], self.circuit)
-
-    def qc_whites(self, code):
-        for i_p in range(self.num_slots):
-            # Place opening quantum circuit
-            self.qc_xor(i_p)
-            self.qc_inv(i_p)
-
-            testcolors = [2 ** self.color_bits - 1] + list(set(code))
-            for i_c in range(1, len(testcolors)):
-                # Place x gates dependent on code
-                self.qc_chk(i_p, (testcolors[i_c] ^ testcolors[i_c - 1]) ^ (2 ** self.color_bits - 1), self.circuit)
-
-                # Place single pin mcrx gate
-                if code[i_p] != testcolors[i_c]:
-                    controls = [self.q[self.color_bits * i_p + i] for i in range(self.color_bits)]
-                    self.circuit.mcrx(np.pi / self.num_slots, controls, self.q[self.color_bits * self.num_slots + 1])
-
-                # Place multiple pin mcrx gates
-                self.qc_tst(i_p, testcolors[i_c], code)
-
-            # Place closing quantum circuit
-            self.qc_chk(i_p, testcolors[-1], self.circuit)
-            self.qc_inv(i_p)
-            self.qc_xor(i_p)
 
     def qc_xor(self, pin):
         for i_p in range(pin):
