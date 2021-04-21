1c3ae129f2d6d7272320593bf67b233971397a7c
0tmar <otmarubbens@live.com>
2021-04-21 22:25:38 +0200

    Quick fix: default secret string back to random

--- src/mastermind/game/algorithms/Buhrman.py
+++ src/mastermind/game/algorithms/Buhrman.py
@@ -180,7 +180,7 @@ class Buhrman(Game, ABC):
     def random_sequence(self):
         # Choose numbers between 0 and pin_amount (do this num_slots times)
         
-        arr = np.array([2, 3, 0, 0])
-        print("\n\nWATCH OUT: RUNNING WITH HARDCODED STRING %s !!!\n\n" % (arr))
-        return arr
-        # return np.random.randint(0, self.pin_amount, size=self.num_slots)
+        # arr = np.array([2, 3, 0, 0])
+        # print("\n\nWATCH OUT: RUNNING WITH HARDCODED STRING %s !!!\n\n" % (arr))
+        # return arr
+        return np.random.randint(0, self.pin_amount, size=self.num_slots)
